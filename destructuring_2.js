48921c94c94bdbc633991c07ccefc2b9f6764524
Sushant <sush1393@gmail.com>
2020-05-10 21:28:26 +0530

    Array and Object destructuring

---
+++ destructuring_2.js
@@ -0,0 +1,15 @@
+// Array and Object destructuring
+
+const classes = [
+    [ 'Chemistry', '9AM', 'Mr. Darnick' ],
+    [ 'Physics', '10:15AM', 'Mrs. Lithun'],
+    [ 'Math', '11:30AM', 'Mrs. Vitalis' ]
+  ];
+  
+  const classesAsObject = classes.map(( [subject, time, teacher] ) => {
+      return [{ subject, time, teacher }];
+  });
+
+  /* Output -   {subject: "Chemistry", time: "9AM", teacher: "Mr. Darnick"}
+                {subject: "Physics", time: "10:15AM", teacher: "Mrs. Lithun"}
+                {subject: "Math", time: "11:30AM", teacher: "Mrs. Vitalis"} */
