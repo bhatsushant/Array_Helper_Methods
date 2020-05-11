5031ed37a73696848344a3d6224a5d1bf188bcbe
Sushant <sush1393@gmail.com>
2020-05-11 15:58:44 +0530

    Generators usinf recurssion

---
+++ generators_4.js
@@ -0,0 +1,32 @@
+// Generators using recurssion
+
+class Comment {
+    constructor(content, children) {
+        this.content = content;
+        this.children = children;
+    }
+
+    *[Symbol.iterator]() {
+        yield this.content;
+        for(let child of this.children) {
+            yield* child;
+        }
+    }
+}
+
+const children = [
+    new Comment('good comment',[]),
+    new Comment('bad comment',[]),
+    new Comment('meh',[])
+];
+
+const tree = new Comment('Great Post!', children);
+
+const values=[];
+
+for(let value of tree) {
+    values.push(value);
+}
+values;
+
+//  ["Great Post!", "good comment", "bad comment", "meh"]
